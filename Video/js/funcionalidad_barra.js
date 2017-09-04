960f2f2ab501ce1bdb11e03684d63ae25f941cc4
Gabriel leal <gaboleal123@gmail.com>
2017-09-04 18:54:37 -0430

    Inserción de Vídeos
    etiqueta video y sus correspondientes atributos.

---
+++ Video/js/funcionalidad_barra.js
@@ -0,0 +1,59 @@
+
+var mivideo, reproducir, barra, progreso, maximo;
+
+maximo=600;
+
+function comenzar(){
+	mivideo=document.getElementById("mivideo");
+	barra=document.getElementById("barra");
+	reproducir=document.getElementById("reproducir");
+	progreso=document.getElementById("progreso");
+
+
+	reproducir.addEventListener("click",clicando,false);
+
+	barra.addEventListener("click",adelantando,false);
+}
+
+
+function clicando(){
+	if ((mivideo.paused==false) && (mivideo.ended==false)){
+		mivideo.pause();
+		reproducir.innerHTML="Play";
+	}
+	else{
+		mivideo.play();
+		reproducir.innerHTML="Pause";
+
+		bucle=setInterval(estado,1000);
+	}
+
+
+	}
+
+  function estado(){
+  	if (mivideo.ended==false) {
+  		var total=parseInt(mivideo.currentTime*maximo/mivideo.duration);
+  		progreso.style.width=total+"px";
+  	}
+  }
+
+    function adelantando(posicion){
+	if ((mivideo.paused==false) && (mivideo.ended==false)){
+		
+		var ratonX=posicion.pageX-barra.offsetLeft;
+
+		var nuevoTiempo=ratonX*mivideo.duration/maximo;
+
+		mivideo.currentTime=nuevoTiempo;
+
+		progreso.style.width=ratonX+"px";
+	}
+	
+
+	}
+
+
+
+
+	window.addEventListener("load",comenzar,false);
